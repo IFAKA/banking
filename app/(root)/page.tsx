929ddf0ce6d19a791a53b668fb038c257a46fc5d
Lichu23 <lisandroxarenax@gmail.com>
2024-09-05 18:47:23 -0300

    feat: add HeaderBox component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Guest'}
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn.firstName || 'Guest'}
+            subtext='Access and manage your account and tx.'
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
