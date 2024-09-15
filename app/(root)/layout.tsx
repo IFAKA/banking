c14ee661a409d3fadb935454fc9aae18e4524e66
IFAKA <facundoarenas29@gmail.com>
2024-09-14 21:43:48 -0300

    feat: add sidebar web

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,31 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isLoggedIn = { firstName: "My", lastName: "Name" };
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={isLoggedIn} />
+
+      <div className="flex flex-col size-full">
+        <div className="root-layout">
+          <Image
+            src={"/icons/logo.svg"}
+            alt="menu logo"
+            height={30}
+            width={30}
+          />
+          <div>
+            <MobileNav user={isLoggedIn} />
+          </div>
+        </div>
         {children}
+      </div>
     </main>
   );
 }
---
+++ components/Sidebar.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const Sidebar = ({ user }: SiderbarProps) => {
+  const pathname = usePathname();
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-2">
+        <Link
+          href={"/"}
+          className="flex mb-12 cursor-pointer items-center gap-2"
+        >
+          <Image
+            alt="Horizon Logo"
+            src={"/icons/logo.svg"}
+            width={34}
+            height={34}
+            className="size-[24px] max=xl:size-14"
+          />
+          <h1 className="sidebar-logo">Horizon</h1>
+        </Link>
+        {sidebarLinks.map((sidebarLink) => {
+          const { imgURL, label, route } = sidebarLink;
+          const isActive =
+            pathname === route || pathname.startsWith(`${route}/`);
+          return (
+            <Link
+              href={route}
+              key={label}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+            >
+              <div className="relative size-4">
+                <Image
+                  className={cn({ "brightness-[3] invert-0": isActive })}
+                  alt={label}
+                  src={imgURL}
+                  fill
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {label}
+              </p>
+            </Link>
+          );
+        })}
+        USER
+      </nav>
+      FOOTER
+    </section>
+  );
+};
+
+export default Sidebar;
