5d23c03aead09d1c45d44827f373c644f86cf491
Lichu23 <lisandroxarenax@gmail.com>
2024-09-22 13:02:57 -0300

    feat: setup signUp in user actions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,10 @@
 "use server";
 
+import { cookies } from "next/headers";
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../server/appwrite";
+import { parseStringify } from "../utils";
+
 export const signIn = async (userData: SignUpParams) => {
   try {
     // validate user credentials
@@ -9,9 +14,32 @@ export const signIn = async (userData: SignUpParams) => {
 };
 
 export const signUp = async (userData: SignUpParams) => {
+  const { email, password, firstName, lastName } = userData;
+  const name = `${firstName} ${lastName}`;
   try {
-    // create user account
+    const { account } = await createAdminClient();
+
+    const newUser = await account.create(ID.unique(), email, password, name);
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify(newUser);
   } catch (error) {
     console.error("Error:   ", error);
   }
 };
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    return null;
+  }
+}
